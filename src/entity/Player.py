49eb4051c58d392d225232a3c71daa7738f09ab3
Andrew Zhang <azhang237@wisc.edu>
2023-10-14 15:19:43 -0500

    fixed getters

--- src/entity/Player.py
+++ src/entity/Player.py
@@ -7,17 +7,17 @@ def __init__(self):
 def setHand(self, hand):
     self.hand = hand
 
-def getHand(self, hand):
-    return hand
+def getHand(self):
+    return self.hand
 
 def setCrib(self, crib):
     self.crib = crib
 
-def getCrib(self, crib):
-    return crib
+def getCrib(self):
+    return self.crib
 
 def setScore(self, score):
     self.score = score
 
-def getScore(self, score):
-    return score
+def getScore(self):
+    return self.score
