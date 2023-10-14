4f6b168424574f660212d8e1be60a91b748c1120
Andrew Zhang <azhang237@wisc.edu>
2023-10-14 14:53:19 -0500

    Player object done

---
+++ src/entity/Player.py
@@ -0,0 +1,23 @@
+
+def __init__(self):
+    self.score = 0
+    self.hand = []
+    self.crib = []
+
+def setHand(self, hand):
+    self.hand = hand
+
+def getHand(self, hand):
+    return hand
+
+def setCrib(self, crib):
+    self.crib = crib
+
+def getCrib(self, crib):
+    return crib
+
+def setScore(self, score):
+    self.score = score
+
+def getScore(self, score):
+    return score
