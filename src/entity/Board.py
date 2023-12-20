686a59c6ff43ca9e97b78561fdc6fd249ed13d73
Dylan Houle <dnhoule@hotmail.com>
2023-12-20 14:18:14 -0600

    small changes

--- src/entity/Board.py
+++ src/entity/Board.py
@@ -5,11 +5,13 @@ from .Card import Card
 class Board:
 
     def __init__(self, mode, num_players, players):
-        self.GAME_MODE = mode
-        self.num_players = num_players
-        self.players = players
+        self.GAME_MODE = mode                        # 0 = Standard, 1 = Performance, 2 = Menos
+        self.num_players = num_players               
+        self.players = self.init_players(players)    # List of strings with names
         self.deck = Deck()
 
+    def init_players(self, players):
+        return [Player(name=player) for player in players]
     
 
     # Return an ordered list of players, ex. [Player 2, Player 0, Player 1]
@@ -33,7 +35,17 @@ class Board:
 
         print(self.players)
 
+        self.deck.shuffle()     # Repopulate the deck
+
         return self.players
+    
+    def deal_cards(self):
+        if self.GAME_MODE == 0:
+            for card in range(6):
+                for player in range(len(self.players)):
+                    player.hand.add(self.deck.get_card())
+                    
+
 
 
 
--- src/run.py
+++ src/run.py
@@ -1,6 +1,10 @@
 from entity.Board import Board
 
+
+
 def main():
+   
+
    board = Board(0, 6, ["Dylan", "Andrew", "Connor", "Bob", "Joe", "Poop"])
 
    board.choose_player_order()
